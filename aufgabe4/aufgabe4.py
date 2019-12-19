fd1121088059d5eed84918ae93846932eb8b8fd7
TiHau <tim.hauenstein97@gmx.de>
2019-12-19 16:56:04 +0100

    a4 test

--- aufgabe4/aufgabe4.py
+++ aufgabe4/aufgabe4.py
@@ -2,13 +2,14 @@ from Robot_Simulator_V3 import labyrinthWorld
 from Robot_Simulator_V3 import Robot
 from Robot_Simulator_V3 import sensorUtilities
 import numpy as np
+# from aufgabe3.aufgabe3_b import followLine
 
 myWorld = labyrinthWorld.buildWorld()
 myRobot = Robot.Robot()
 myWorld.setRobot(myRobot, [2, 18, 0])
 
 FOLLOW_RIGHT = True # False for left hand
-SHOWLINES = "right" # left, alternate, none, all
+SHOWLINES = "all" # left, alternate, none, all
 
 myKeyboardController = myWorld.getKeyboardController()
 
@@ -20,49 +21,20 @@ while True:
     myRobot.move(motion)
     dists = myRobot.sense()
     directions = myRobot.getSensorDirections()
-    lines_l = sensorUtilities.extractSegmentsFromSensorData(dists, directions)
-    
-    left_list = []
-    right_list = []
-    if len(lines_l) > 1:
-        for line in lines_l:
-            if line[0][1] > 0 and line[1][1] > 0:
-                left_list.append(line)
-            else:
-                right_list.append(line)
-    
-    #TODO get line that is the most parallel to the robots view (smallest theta)
-    line_candidates = []
-    if FOLLOW_RIGHT:
-        line_candidates = right_list
-    else:
-        line_candidates = left_list
-    #print(line_candidates)
-    for line in line_candidates:
-        angle = np.arctan2(line[0][1], line[1][1])
-        print(angle) #TODO angle not correct!
-        pass
-    
-    #TODO translate that line towards the robot, by the distance from the robot
-    #TODO follow line
-    
-    if SHOWLINES is not "none":
-        right_list_g = sensorUtilities.transformPolylinesL2G(right_list, myWorld.getTrueRobotPose())
-        left_list_g = sensorUtilities.transformPolylinesL2G(left_list, myWorld.getTrueRobotPose())
-        lines_g = sensorUtilities.transformPolylinesL2G(lines_l, myWorld.getTrueRobotPose())
-        if SHOWLINES is "right":
-            myWorld.drawPolylines(right_list_g, "pink")
-        elif SHOWLINES is "left":
-            myWorld.drawPolylines(left_list_g, "light green")
-        elif SHOWLINES is "alternate":
-            if alternate:
-                myWorld.drawPolylines(right_list_g, "pink")
-                alternate = False
-            else:
-                myWorld.drawPolylines(left_list_g, "light green")
-                alternate = True
-        elif SHOWLINES is "all":
-            myWorld.drawPolylines(lines_g, "white")
-        
+    line = sensorUtilities.extractSegmentsFromSensorData(dists[4:8], directions[4:8])
+
+    crash_dist = 100
+    if dists[13] and dists[14]:
+        crash_dist = (dists[13]+dists[14])/2
+    print(crash_dist)
+
+    if len(line) > 0:
+        line = line[0]
+        print(line)
+
+        offset = 1
+        line = [[line[0][0], line[0][1] + offset], [line[1][0], line[1][1] + offset]]
+        border_line = sensorUtilities.transformPolylinesL2G([line], myWorld.getTrueRobotPose())
+        myWorld.drawPolylines(border_line, "light green")
 
 myWorld.close(False)
