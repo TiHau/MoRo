6c619581c4c3d11e68a94d7e88828e3bdd09d24e
TiHau <tim.hauenstein97@gmx.de>
2020-01-14 14:53:54 +0100

    teil1

---
+++ aufgabe6/aufgabe6_1.py
@@ -0,0 +1,23 @@
+import numpy as np
+from Robot_Simulator_V3 import simpleWorld
+from Robot_Simulator_V3 import Robot
+
+
+def curveDrive(robot, world, v=1, r=4, deltaTheta=-np.pi, n=100):
+    omega = v / r * np.sign(deltaTheta)
+    robot.setTimeStep(abs(deltaTheta / omega))
+
+    poses = [world.getTrueRobotPose()]
+
+    for t in range(n):
+        robot.move([v / n, omega / n])
+        poses.append(world.getTrueRobotPose())
+    return poses
+
+
+if __name__ == "__main__":
+    myWorld = simpleWorld.buildWorld()
+    myRobot = Robot.Robot()
+    myWorld.setRobot(myRobot, [4, 4, np.pi / 2])
+    print(curveDrive(myRobot, myWorld))
+    myWorld.close()
